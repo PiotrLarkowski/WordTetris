d119832c436869dc10f3b60abcaf95eea659f190
Piotr <P.Larkowski90@gmail.com>
2024-09-18 20:10:45 +0200

    Paint main viev of game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,12 +3,16 @@ import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
 
+    static final int WIDTH = 600;
+    static final int HEIGHT = 900;
+
     Thread gameThread;
     final int FPS = 60;
+    PlayManager pm = new PlayManager();
 
     public GamePanel(){
 
-        this.setPreferredSize(new Dimension(640, 900));
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.white);
         this.setLayout(null);
 
@@ -42,10 +46,12 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update(){
-
+        pm.update();
     }
 
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        pm.draw(g2);
     }
 }
---
+++ src/PlayManager.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class PlayManager {
+
+    final int WIDTH = 400;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+
+    public PlayManager(){
+
+        left_x = (GamePanel.WIDTH/2)-WIDTH/2;
+        right_x = left_x + WIDTH;
+        top_y = 100;
+        bottom_y = top_y + HEIGHT;
+
+    }
+
+    public void update(){
+
+    }
+
+    public void draw(Graphics2D g2){
+
+        g2.setColor(Color.black);
+        g2.setStroke(new BasicStroke(4f));
+        g2.drawRect(left_x-4, top_y - 4, WIDTH+8, HEIGHT + 8);
+        g2.drawRect(left_x+8, top_y+8, WIDTH-16, HEIGHT-16);
+
+    }
+
+}
